4f3daa17f3f317c6c5559735f51f3275fc326b0e
Ahsan Mahmood <aoneahsan@gmail.com>
2025-08-08 13:31:36 +0500

    working on app

---
+++ functions/src/config/firebase.ts
@@ -0,0 +1,13 @@
+import * as admin from 'firebase-admin';
+
+// Initialize Firebase Admin
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+export const db = admin.firestore();
+export const auth = admin.auth();
+export const storage = admin.storage();
+export const messaging = admin.messaging();
+
+export default admin;
